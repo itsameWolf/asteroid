3ac48464ca9e21a855407a890aedc7fb76eed3f8
lupo manes <lupo961@gmail.com>
2023-06-29 17:11:48 +0100

    Added asteroid showers with semi-random trajectories sizes and speeds

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,44 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+use bevy_turborand::prelude::*;
+use std::ops::Mul;
+use std::time::Duration;
 
 const SPACESHIP_SIZE: Vec3 = Vec3::new(10.0, 20.0, 0.0);
+const SPACESHIP_THRUST: f32 = 1500.0;
+const SPACESHIP_TORQUE: f32 = 1.5;
 
 const PROJECTILE_SIZE: Vec3 = Vec3::new(1.0, 5.0, 0.0);
 const PROJECTILE_SPEED: f32 = 250.0;
 
 const CANNON_TRANSFORM: Transform = Transform::from_xyz(0.0, 37.0, 0.0);
 
-const SPACESHIP_THRUST: f32 = 1500.0;
-const SPACESHIP_TORQUE: f32 = 1.5;
+const MAX_ASTEROID_RADIUS: f32 = 40.0;
+const MIN_ASTEROID_RADIUS: f32 = 5.0;
+const MAX_ASTEROID_SPEED: f32 = 500.0;
+const MIN_ASTEROID_SPEED: f32 = 150.0;
+
+const ASTEROID_FREQUENCY: f32 = 1.5;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
+        .insert_resource(AsteroidCooldown {
+            timer: Timer::from_seconds(ASTEROID_FREQUENCY, TimerMode::Repeating),
+        })
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        //.add_plugin(RapierDebugRenderPlugin::default())
+        .add_plugin(RngPlugin::default())
+        .add_plugin(RapierDebugRenderPlugin::default())
         .add_event::<ShootEvent>()
+        .add_event::<AsteroidEvent>()
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_spaceship)
         .add_system(spaceship_controller)
         .add_system(spawn_projectile)
+        .add_system(asteroid_shower)
+        .add_system(spawn_asteroid)
         .run()
 }
 
@@ -106,6 +122,66 @@ fn spawn_projectile(
     cooldown.timer.tick(time_step.period);
 }
 
+fn spawn_asteroid(
+    mut commands: Commands,
+    mut asteroid_event: EventReader<AsteroidEvent>,
+    asset_server: Res<AssetServer>,
+) {
+    for asteroid in asteroid_event.iter() {
+        let asteroid_trans = Transform::from_xyz(asteroid.0.x, asteroid.0.y, asteroid.0.z)
+            .looking_at(asteroid.1, Vec3::Z);
+        let force_vec = asteroid_trans.forward();
+        commands
+            .spawn(SpriteBundle {
+                texture: asset_server.load("asteroid.png"),
+                sprite: Sprite {
+                    custom_size: Some(Vec2::from([asteroid.3 * 2.0, asteroid.3 * 2.0])),
+                    ..default()
+                },
+                transform: asteroid_trans,
+                ..default()
+            })
+            .insert(Asteroid)
+            .insert(RigidBody::KinematicVelocityBased)
+            .insert(Collider::ball(asteroid.3))
+            .insert(Velocity {
+                linvel: force_vec.truncate().mul(asteroid.2),
+                angvel: 0.0,
+            });
+    }
+}
+
+fn asteroid_shower(
+    mut asteroid_event: EventWriter<AsteroidEvent>,
+    mut asteroid_timer: ResMut<AsteroidCooldown>,
+    mut rng: ResMut<GlobalRng>,
+) {
+    if asteroid_timer.timer.finished() {
+        let origin = Vec3::new(
+            {
+                let x = rng.f32() - 0.5;
+                x * 100.0 + x.signum() * 500.0
+            },
+            {
+                let y = rng.f32() - 0.5;
+                y * 100.0 + y.signum() * 300.0
+            },
+            0.0,
+        );
+        let target = Vec3::new(rng.f32() * 10.0 - 5.0, rng.f32() * 10.0 - 5.0, 0.0);
+        let speed = rng.f32() * (MAX_ASTEROID_SPEED - MIN_ASTEROID_SPEED) + MIN_ASTEROID_SPEED;
+        let radius = rng.f32() * (MAX_ASTEROID_RADIUS - MIN_ASTEROID_RADIUS) + MIN_ASTEROID_RADIUS;
+        println!(
+            "Asteroid spawned from {:?} going to {:?} at {:?}",
+            origin, target, speed
+        );
+        asteroid_event.send(AsteroidEvent(origin, target, speed, radius));
+    }
+    asteroid_timer
+        .timer
+        .tick(Duration::from_secs_f32(rng.f32() % 0.05));
+}
+
 fn spaceship_controller(
     keyboard_input: Res<Input<KeyCode>>,
     mut spaceship: Query<(&mut ExternalForce, &Transform), With<Spaceship>>,
@@ -147,9 +223,6 @@ struct Asteroid;
 #[derive(Component)]
 struct Projectile;
 
-#[derive(Default)]
-struct ShootEvent;
-
 #[derive(Component)]
 struct Cannon;
 
@@ -157,3 +230,14 @@ struct Cannon;
 struct CannonCooldown {
     timer: Timer,
 }
+
+#[derive(Resource)]
+struct AsteroidCooldown {
+    timer: Timer,
+}
+
+#[derive(Default)]
+struct ShootEvent;
+
+#[derive(Default)]
+struct AsteroidEvent(Vec3, Vec3, f32, f32);
