cfdf1780fd3819dce6e59d3313915848377d7855
lupo manes <lupo961@gmail.com>
2023-06-29 12:02:13 +0100

    refactored and prettified

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
-const SPACESHIP_SIZE: Vec3 = Vec3::new(20.0, 50.0, 0.0);
-const SPACESHIP_COLOR: Color = Color::rgb(0.0, 0.5, 0.5);
+const SPACESHIP_SIZE: Vec3 = Vec3::new(10.0, 20.0, 0.0);
 
-const SPACESHIP_SPEED: f32 = 3000.0;
-const SPACESHIP_ANGULAR_SPEED: f32 = 8.0;
+const PROJECTILE_SIZE: Vec3 = Vec3::new(1.0, 5.0, 0.0);
+const PROJECTILE_SPEED: f32 = 250.0;
+
+const CANNON_TRANSFORM: Transform = Transform::from_xyz(0.0, 37.0, 0.0);
+
+const SPACESHIP_THRUST: f32 = 1500.0;
+const SPACESHIP_TORQUE: f32 = 1.5;
 
 fn main() {
     App::new()
@@ -13,7 +17,7 @@ fn main() {
         .insert_resource(FixedTime::new_from_secs(1.0 / 60.0))
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        .add_plugin(RapierDebugRenderPlugin::default())
+        //.add_plugin(RapierDebugRenderPlugin::default())
         .add_event::<ShootEvent>()
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_spaceship)
@@ -28,10 +32,20 @@ fn spawn_camera(mut commands: Commands) {
 
 fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
-        .spawn(Spaceship)
+        .spawn(SpriteBundle {
+            texture: asset_server.load("spaceship.png"),
+            sprite: Sprite {
+                custom_size: Some(Vec2::from([
+                    SPACESHIP_SIZE[0] * 4.0,
+                    SPACESHIP_SIZE[1] * 3.0,
+                ])),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Spaceship)
         .insert(RigidBody::Dynamic)
         .insert(Collider::capsule_y(SPACESHIP_SIZE[1], SPACESHIP_SIZE[0]))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, 500.0, 0.0)))
         .insert(GravityScale(0.0))
         .insert(Sleeping::disabled())
         .insert(ExternalForce {
@@ -42,46 +56,47 @@ fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
             linear_damping: 1.0,
             angular_damping: 1.0,
         })
-        .insert(SpriteBundle {
-            texture: asset_server.load("spaceship.png"),
-            sprite: Sprite {
-                custom_size: Some(Vec2::from([
-                    SPACESHIP_SIZE[0] * 3.0,
-                    SPACESHIP_SIZE[1] * 2.0,
-                ])),
-                ..default()
-            },
-            ..default()
-        })
         .with_children(|parent| {
             parent
                 .spawn(Cannon)
                 .insert(CannonCooldown {
                     timer: Timer::from_seconds(0.5, TimerMode::Once),
                 })
-                .insert(TransformBundle::from(Transform::from_xyz(0.0, 80.0, 0.0)));
+                .insert(TransformBundle::from(Transform::from(CANNON_TRANSFORM)));
         });
 }
 
 fn spawn_projectile(
     mut commands: Commands,
-    mut cannon_state: Query<(&GlobalTransform, &mut CannonCooldown), With<Cannon>>,
+    mut cannon: Query<(&GlobalTransform, &mut CannonCooldown), With<Cannon>>,
     mut shoot_event: EventReader<ShootEvent>,
     time_step: Res<FixedTime>,
+    asset_server: Res<AssetServer>,
 ) {
-    let (trans, mut cooldown) = cannon_state.single_mut();
+    let (trans, mut cooldown) = cannon.single_mut();
     if cooldown.timer.finished() {
         for _ in shoot_event.iter() {
             let rot = trans.up();
             commands
-                .spawn(Projectile)
+                .spawn(SpriteBundle {
+                    texture: asset_server.load("laser.png"),
+                    sprite: Sprite {
+                        custom_size: Some(Vec2::from([
+                            SPACESHIP_SIZE[0] * 3.0,
+                            SPACESHIP_SIZE[1] * 2.0,
+                        ])),
+                        ..default()
+                    },
+                    transform: (*trans).into(),
+                    ..default()
+                })
+                .insert(Projectile)
                 .insert(RigidBody::KinematicVelocityBased)
-                .insert(Collider::cuboid(1.0, 10.0))
-                .insert(*trans)
+                .insert(Collider::capsule_y(PROJECTILE_SIZE[1], PROJECTILE_SIZE[0]))
                 .insert(Velocity {
                     linvel: Vec2 {
-                        x: rot[0] * 100.0,
-                        y: rot[1] * 100.0,
+                        x: rot[0] * PROJECTILE_SPEED,
+                        y: rot[1] * PROJECTILE_SPEED,
                     },
                     angvel: 0.0,
                 });
@@ -116,10 +131,10 @@ fn spaceship_controller(
         shoot_event.send_default();
     }
 
-    force.torque = twist * SPACESHIP_ANGULAR_SPEED * time_step.period.as_secs_f32();
+    force.torque = twist * SPACESHIP_TORQUE * time_step.period.as_secs_f32();
     force.force = Vec2::new(
-        thrust * SPACESHIP_SPEED * time_step.period.as_secs_f32() * vector[0],
-        thrust * SPACESHIP_SPEED * time_step.period.as_secs_f32() * vector[1],
+        thrust * SPACESHIP_THRUST * time_step.period.as_secs_f32() * vector[0],
+        thrust * SPACESHIP_THRUST * time_step.period.as_secs_f32() * vector[1],
     );
 }
 
